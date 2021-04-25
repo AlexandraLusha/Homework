ea47f92abcdf0d556c669d84c6fec34a27e05a3f
AlexandraLusha <lusha82000@mail.ru>
2021-04-25 20:39:45 +0300

    Add files via upload

---
+++ Orange.java
@@ -0,0 +1,11 @@
+package Java_Core.Lesson03;
+
+public class Orange extends Fruit {
+    Orange() {
+        super(1.5f);
+    }
+    @Override
+    public String toString(){
+        return "апельсины";
+    }
+}
